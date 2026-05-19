8596ba9f87f6105e80837a140c2685d00758dace
Alvaro Lozano de Osma <74834169+Alozanodeosma@users.noreply.github.com>
2026-05-19 12:08:31 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [timer,setTimer] = useState(5);
+
+  useEffect(() => {
+    if (timer === 0) return;
+    const id = setTimeout(() => {
+      setTimer(t => t - 1);
+    }, 1000);
+    return () => clearTimeout(id);
+  }, [timer]);
+  
+  const [password, setPassword]=useState(`Click 'Generate OTP' to get a one-time password`);
+  const [isClicked, setIsClicked] = useState(false);
+
+  function createOTP(num){
+    return parseInt(Math.random()*10**num);
+  }
+
+  const generateOTP = ()=>{
+    setPassword(createOTP(6));
+    setIsClicked(true);
+    setTimer(5);
+    }
+
+  if(!isClicked){
+    return(
+    <div className="container">
+      <h1 id="otp-title">OTP Generator</h1>
+      <h2 id="otp-display">{password}</h2>
+      <p id="otp-timer" aria-live="polite"></p>
+      <button id="generate-otp-button" onClick={generateOTP}>Generate OTP</button>
+    </div>
+  )
+  }
+  return(
+    <div className="container">
+      <h1 id="otp-title">OTP Generator</h1>
+      <h2 id="otp-display">{password}</h2>
+      <p id="otp-timer" aria-live="polite">{timer ? `Expires in: ${timer} seconds` : `OTP expired. Click the button to generate a new OTP.`}</p>
+      <button id="generate-otp-button" onClick={generateOTP} disabled={timer > 0}>Generate OTP</button>
+    </div>
+  )
+};
+export default App
